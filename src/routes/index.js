87dfb1000d9880b0d8783c90e0e75c48d63fae5e
vitordealbs <vitoralbuquerque@alu.ufc.br>
2025-07-25 18:40:54 -0300

    fazendo get para adm

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,13 +3,14 @@ import authRoutes from "./authRoutes.js";
 import userRoutes from "./userRoutes.js";
 import equipmentRoutes from "./equipmentRoutes.js";
 import loanRoutes from "./loanRoutes.js";
+import admRoutes from "./admRoutes.js";
 import { authMiddleware } from "../middlewares/authMiddleware.js";
 
 const router = Router();
-
 router.use("/auth", authRoutes);
 router.use("/users", authMiddleware, userRoutes);
 router.use("/equipments", authMiddleware, equipmentRoutes);
 router.use("/loans", loanRoutes);
+router.use("/admin", authMiddleware, admRoutes);
 
 export default router;
